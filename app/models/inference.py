20670ff813e464597aba6deef8560ed2025cfd8e
jinyang628 <chenjinyang4192@gmail.com>
2024-07-17 10:01:02 -0700

    Update

--- app/models/inference.py
+++ app/models/inference.py
@@ -22,8 +22,8 @@ class HttpMethodResponse(BaseModel):
     application: ApplicationContent
     table_name: str
     inserted_rows: Optional[list[dict[str, Any]]] = None
-    filter_conditions: Optional[list[dict[str, Any]]] = None
-    updated_data: Optional[list[dict[str, Any]]] = None
+    filter_conditions: Optional[dict[str, Any]] = None
+    updated_data: Optional[dict[str, Any]] = None
     
 class InferenceResponse(BaseModel):
     response: list[HttpMethodResponse]
